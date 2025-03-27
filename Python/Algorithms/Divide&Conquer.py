01379818c0630b769abe4d96c5fbbe1086f2530f
Blue-Killer87 <janvyhnanek04@seznam.cz>
2025-03-27 15:50:27 +0100

    .

---
+++ Python/Algorithms/Divide&Conquer.py
@@ -0,0 +1,25 @@
+class DivideNConquer:
+    def __init__(self):
+        index = 10
+        self.itemlist = []
+        for i in range(index):
+            self.itemlist.append(i)
+
+    def find(self, value):
+        split = self.itemlist[len(self.itemlist)//2]
+        if split < value:
+            print(split)
+            self.itemlist[:split] = ""
+            self.find(value)
+
+        elif split > value:
+            print(split)
+            self.itemlist[split:] = ""
+            self.find(value)
+
+        else:
+            print(split)
+
+
+d = DivideNConquer()
+d.find(3)
