82da034c9016f52363f54cefe233d301a0f84d14
Blue-Killer87 <janvyhnanek04@seznam.cz>
2025-03-29 01:52:14 +0100

    .

--- Python/Random/Random2.py
+++ Python/Random/Random2.py
@@ -15,10 +15,8 @@ class Random():
             self.length = length
             self.modulator = self.length*69
             self.EntropyGen()
-            while self.iteration < 5*len(self.randomList):
+            while self.iteration < 150*len(self.randomList):
                 self.EntropyGen()
-
-            
             self.assignNumber()
         return self.randomList, self.TotalSame
 
@@ -31,22 +29,30 @@ class Random():
         
 
     def assignNumber(self):
-        self.previous = self.random
-        self.random = (self.value*self.length*self.time)%10
-        self.random = int(round(self.random, 0))
-        if self.previous == self.random:
-            self.TotalSame += 1
-        if self.random < 0:
-            self.random *= -1
-        self.randomList.append(self.random)
-        self.iteration = 0
-        return
+        try:
+            self.previous = self.random
+            self.random = (self.value*self.length*self.time)%10
+            self.random = int(round(self.random, 0))
+            if self.previous == self.random:
+                self.TotalSame += 1
+                self.random = "!-!"
+            if self.random < 0:
+                self.random *= -1
+            self.randomList.append(self.random)
+            print(self.random, end=" ", flush=True)
+            self.iteration = 0
+            return
+        except:
+            self.randomList.append(self.random)
+            print(self.random, end=" ", flush=True)
+            self.iteration = 0
+            return
 
 
 length = int(input('Set how many random numbers you want: '))
 random = Random().getRandom(length)
 
-print(f"Random numbers: {random[0]}")
+#print(f"Random numbers: {random[0]}")
 print(f"\n\nAmount of numbers: {length}")
 print(f"Amount of duplicate numbers: {random[1]}")
 print(f"Percentage of randomness: {100-(random[1]/length)*100}")
