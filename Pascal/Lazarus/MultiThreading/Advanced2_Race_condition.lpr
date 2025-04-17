1a29f6558a0dc139382209488889aa335ea12246
Blue-Killer87 <janvyhnanek04@seznam.cz>
2025-04-17 13:38:15 +0200

    Added other multithreading programs

---
+++ Pascal/Lazarus/MultiThreading/Advanced2_Race_condition.lpr
@@ -0,0 +1,10 @@
+program Advanced2_Race_condition;
+
+uses cthreads, classes, dodavatel, odberatel;
+
+type
+
+
+begin
+end.
+
---
+++ Pascal/Lazarus/MultiThreading/odberatel.pas
@@ -0,0 +1,13 @@
+unit odberatel;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+implementation
+
+end.
+
